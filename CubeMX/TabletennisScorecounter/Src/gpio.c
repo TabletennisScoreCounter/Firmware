83bb8a183edc43e915132c8c31a415ad9b83e56d
ShujiMikami <carbonized.pan@gmail.com>
2017-12-10 20:40:05 +0900

    青ボタンをRisingEdgeに戻した

--- CubeMX/TabletennisScorecounter/Src/gpio.c
+++ CubeMX/TabletennisScorecounter/Src/gpio.c
@@ -79,7 +79,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
