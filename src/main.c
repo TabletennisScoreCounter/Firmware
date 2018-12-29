41df5d4e5fb1d9592356e4883922477d79210403
ShujiMikami <carbonized.pan@gmail.com>
2018-12-29 13:33:19 +0900

    クロックコンフィグが2回回っていたのを訂正

--- src/main.c
+++ src/main.c
@@ -77,7 +77,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM2_Init();
-  SystemClock_Config();
   MX_TIM3_Init();
   MX_TIM4_Init();
   MX_TIM5_Init();
