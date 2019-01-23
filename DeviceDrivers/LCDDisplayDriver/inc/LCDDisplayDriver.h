147263c0961c6789b5eb5c8345d1a964e7cafde8
CO\MM07860 <shuji_mikami@murata.com>
2019-01-23 12:58:30 +0900

    extern Cを追記

--- DeviceDrivers/LCDDisplayDriver/inc/LCDDisplayDriver.h
+++ DeviceDrivers/LCDDisplayDriver/inc/LCDDisplayDriver.h
@@ -7,10 +7,20 @@
 
 #ifndef LCDDISPLAYDRIVER_INC_LCDDISPLAYDRIVER_H_
 #define LCDDISPLAYDRIVER_INC_LCDDISPLAYDRIVER_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "i2c.h"
 
 void initialize_LCDDisplayDriver();
 void setChar_LCDDisplayDriver(uint8_t* str, uint8_t length, uint8_t line);
 void clearChar_LCDDisplayDriver();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif /* LCDDISPLAYDRIVER_INC_LCDDISPLAYDRIVER_H_ */
